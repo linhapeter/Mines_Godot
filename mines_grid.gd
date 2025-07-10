500af8d6dfcf8be309d5b8408a9b67f10a28b79c
linhapeter <linha.peter.jr@gmail.com>
2025-07-10 19:49:39 +0200

    Implementing flag placement

--- mines_grid.gd
+++ mines_grid.gd
@@ -30,6 +30,8 @@ const TILE_SET_ID = 0
 const DEFAULT_LAYER = 0
 
 var cells_with_mines = []
+var cells_with_flags = []
+var flags_placed = 0
 var cells_checked_recursively = []
 var is_game_finished = false
 
@@ -58,7 +60,10 @@ func place_mines():
 		set_cell(DEFAULT_LAYER, cell, TILE_SET_ID, CELLS.DEFAULT, 1)
 		
 func _input(event: InputEvent):
-
+	
+	if is_game_finished:
+		return
+	
 	if event is not InputEventMouseButton || !event.pressed:
 		return
 	
@@ -67,9 +72,11 @@ func _input(event: InputEvent):
 	if event.button_index == 1:
 		on_cell_clicked(clicked_cell_coord)
 	elif event.button_index == 2:
-		print("Place Flag")
+		place_flag(clicked_cell_coord)
 
 func on_cell_clicked(cell_coord):
+	if cells_with_flags.any(func (cell): return cell.x == cell_coord.x && cell.y == cell_coord.y):
+		return
 	if cells_with_mines.any(func (cell): return cell.x == cell_coord.x && cell.y == cell_coord.y):
 		
 		lose(cell_coord)
@@ -126,3 +133,41 @@ func lose(cell_coord):
 		set_tile_cell(cell, "MINE")
 		
 	set_tile_cell(cell_coord, "MINE_RED")
+
+func place_flag(cell_coord):
+	var tile_data = get_cell_tile_data(DEFAULT_LAYER, cell_coord)
+	var atlas_coordinates = get_cell_atlas_coords(DEFAULT_LAYER, cell_coord)
+	var is_empty_cell = atlas_coordinates == Vector2i(2,2)
+	var is_flag_cell = atlas_coordinates == Vector2i(0,2)
+	
+	if !is_empty_cell && !is_flag_cell:
+
+		return
+		
+	if is_flag_cell:
+		set_tile_cell(cell_coord, "DEFAULT")
+		cells_with_flags.erase(cell_coord)
+		flags_placed -= 1
+
+	elif is_empty_cell:
+		if flags_placed == number_of_mines:
+			return
+			
+		flags_placed += 1
+		set_tile_cell(cell_coord, "FLAG")
+		cells_with_flags.append(cell_coord)
+		
+	flag_change.emit(flags_placed)
+		
+	var count = 0
+	for flag_cell in cells_with_flags:
+		for mine_cell in cells_with_mines:
+			if flag_cell.x == mine_cell.x && flag_cell.y == mine_cell.y:
+				count += 1
+				
+	if count == cells_with_mines.size():
+		win()
+		
+func win():
+	is_game_finished = true
+	game_won.emit()
